34485d4c56f3336f0dab1a51e7d62f33049a6027
Erin Borders <borders16@gmail.com>
2019-08-12 11:43:12 -0400

    created readme, django heroku and procfile

---
+++ README.md
@@ -0,0 +1,5 @@
+# Overview
+
+# Links
+
+- Project Board: https://trello.com/b/m69SUYmk/ment
--- ment_project/settings.py
+++ ment_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_extensions',
+    'ment_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,11 @@ WSGI_APPLICATION = 'ment_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mentuser_database',
+        'USER': 'mentuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
@@ -118,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
