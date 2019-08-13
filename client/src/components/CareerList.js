d5016d911de5345add79a54e7be6146def91b070
Erin Borders <borders16@gmail.com>
2019-08-12 23:33:00 -0400

    installed axios and react router dom, added client side routes

---
+++ client/src/components/CareerList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CareerList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
