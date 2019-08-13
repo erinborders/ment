c16e15b228d91dde002014eb671432399ddd27e2
Erin Borders <borders16@gmail.com>
2019-08-12 23:14:40 -0400

    added app urls

--- ment_app/urls.py
+++ ment_app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('careers', views.CareerView)
+router.register('mentors', views.MentorView)
+router.register('blogposts', views.BlogPostView)
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
