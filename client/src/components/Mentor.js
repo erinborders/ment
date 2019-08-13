40c9973881f3e533c8be60a8635d75fd78af5f94
Erin Borders <borders16@gmail.com>
2019-08-13 09:36:34 -0400

    created mentor and post detail components

---
+++ client/src/components/Mentor.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+export default class Mentor extends Component {
+    state = {
+        mentor: {},
+        posts: []
+    }
+
+    componentDidMount(){
+        this.fetchMentor()
+    }
+
+    fetchMentor = () => {
+        axios.get(`/api/v1/mentors/${this.props.match.params.id}`)
+            .then(mentor => {
+                this.setState({
+                    mentor: mentor.data,
+                    posts: mentor.data.posts
+                })
+            })
+    }
+
+    
+    render() {
+        let postList = this.state.posts.map(post => {
+            return(
+                <div key={post.id}>
+                    <Link to={`/posts/${post.id}`} >{post.title}</Link>
+                    <p>{post.description}</p>
+                </div>
+            )
+        })
+
+        return (
+            <div>
+                <img src={this.state.mentor.image_url} />
+                <h2>{this.state.mentor.name}</h2>
+                <p>{this.state.mentor. profession}</p>
+                <p>{this.state.mentor.advice_topics}</p>
+                <h1>Blog Posts</h1>
+                    {postList}
+            </div>
+        )
+    }
+}
---
+++ client/src/components/Post.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Post extends Component {
+    state = {
+        post: {}
+    }
+
+    componentDidMount() {
+        this.fetchPost()
+    }
+
+    fetchPost = () => {
+        axios.get(`/api/v1/posts/${this.props.match.params.id}`)
+            .then(post => {
+                this.setState({post})
+            })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.post.title}</h1>
+                <p>{this.state.post.date}</p>
+                <h3>{this.state.post.description}</h3>
+                <p>{this.state.post.body}</p>
+            </div>
+        )
+    }
+}
