b7d1255fa3d3994e4f1728cb56f0551201c05afa
Erin Borders <borders16@gmail.com>
2019-08-12 22:35:29 -0400

    added models and made migrations

--- ment_app/models.py
+++ ment_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Career(models.Model):
+    career_field = models.CharField(max_length=500)
+    occupations = models.CharField(max_length=500)
+    description = models.TextField()
+    skills = models.CharField(max_length=500)
+    education = models.CharField(max_length=500)
+    employers = models.CharField(max_length=500)
+    job_data = models.TextField()
+
+    def __str__(self):
+        return self.career_field
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=500)
+    profession = models.CharField(max_length=500)
+    # TO DO: change advice field to choices field
+    advice_topics = models.CharField(max_length=500)
+    # TO DO: change image url to image field
+    image_url = models.CharField(max_length=500)
+    company = models.CharField(max_length=500)
+    # TO DO: change email to email field
+    email = models.CharField(max_length=500)
+    career = models.ForeignKey(Career, on_delete=models.CASCADE, related_name='mentors')
+
+    def __str__(self):
+        return self.profession
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    body = models.TextField()
+    # TO DO: change date to date field
+    date = models.CharField(max_length=500)
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
