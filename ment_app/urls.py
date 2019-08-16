31995da793bac8f3d98ca38c661db138b51beab3
Erin Borders <borders16@gmail.com>
2019-08-16 14:49:15 -0400

    switched out hard coded data in get job centers view to variables

--- ment_app/urls.py
+++ ment_app/urls.py
@@ -6,10 +6,10 @@ router = routers.DefaultRouter()
 router.register('careers', views.CareerView)
 router.register('mentors', views.MentorView)
 router.register('blogposts', views.BlogPostView)
-# TO DO: ADD ROUTE FOR YOUTH PROGRAMS?
 
 urlpatterns = [
     path('', include(router.urls)),
     # backend url, doesn't have to match client-side routes
-    path('youth-programs/', views.get_youth_programs)
+    path('youth-programs/', views.get_youth_programs),
+    path('job-centers/', views.get_job_centers)
 ]
--- ment_app/views.py
+++ ment_app/views.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets
 from django.shortcuts import render
 from .serializers import CareerSerializer, MentorSerializer, BlogPostSerializer
 from .models import Career, Mentor, BlogPost
-from .forms import YouthProgramForm
 from django.http import JsonResponse
 import requests 
 from django.conf import settings
@@ -28,6 +27,23 @@ def get_youth_programs(request):
     key = settings.CAREERONESTOP_KEY
     url = 'https://api.careeronestop.org/v1/youthprogramfinder/{userId}/{location}/{radius}/0/0/0/10'.format(userId=userId, location=location, radius=radius)
     
+    headers = {'Authorization': 'Bearer ' + key}
+
+    r = requests.get(url, headers=headers).json()
+
+    if r == None:
+        print('Nothing returned')
+    else:
+        print(r)
+    return JsonResponse(r, safe=False)
+
+def get_job_centers(request):
+    location = request.GET['location']
+    radius = request.GET['radius']
+    userId = settings.CAREERONESTOP_ID
+    key = settings.CAREERONESTOP_KEY
+    url = 'https://api.careeronestop.org/v1/ajcfinder/{userId}/{location}/{radius}/0/0/0/0/0/0/0/10'.format(userId=userId, location=location, radius=radius)
+
     headers = {'Authorization': 'Bearer ' + key}
 
     r = requests.get(url, headers=headers).json()
